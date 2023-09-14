532a239de1adfe8a5c46010ecf34e13d2ff55b56
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-09-14 11:24:55 +0530

    Basic Input Form

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -1,12 +1,63 @@
-import { Input, Stack } from "@chakra-ui/react";
+import {
+  Input,
+  Stack,
+  Container,
+  FormLabel,
+  InputGroup,
+  Box,
+  Select,
+} from "@chakra-ui/react";
 import React from "react";
 
 export default function InputForm() {
   return (
-    <Box>
-      <Stack>
-        <Input type="text" placeholder="Something" />
+    <Container>
+      <Stack mt={2}>
+        <InputGroup>
+          <FormLabel fontSize={"larger"} mx={1.5}>
+            Latitude:
+          </FormLabel>
+          <Input type="text" placeholder="Latitude" />
+          <FormLabel fontSize={"larger"} mx={1.5}>
+            Longitude:
+          </FormLabel>
+          <Input type="text" placeholder="Longitude" />
+        </InputGroup>
+        <Box>
+          <FormLabel>Housing Median Age :</FormLabel>
+          <Input type="number" placeholder="99" />
+        </Box>
+        <Box>
+          <FormLabel>Total rooms :</FormLabel>
+          <Input type="number" placeholder="99" />
+        </Box>
+        <Box>
+          <FormLabel>Total bedrooms :</FormLabel>
+          <Input type="number" placeholder="99" />
+        </Box>
+        <Box>
+          <FormLabel>Population :</FormLabel>
+          <Input type="number" placeholder="99" />
+        </Box>
+        <Box>
+          <FormLabel>Households :</FormLabel>
+          <Input type="number" placeholder="126" />
+        </Box>
+        <Box>
+          <FormLabel>Median Income :</FormLabel>
+          <Input type="number" placeholder="8.3252" />
+        </Box>
+        <Box>
+          <FormLabel>Ocean Proximity:</FormLabel>
+          <Select>
+            <option>NEAR BAY</option>
+            <option>NEAR OCEAN</option>
+            <option>less than 1hr OCEAN</option>
+            <option>INLAND</option>
+            <option>ISLAND</option>
+          </Select>
+        </Box>
       </Stack>
-    </Box>
+    </Container>
   );
 }
--- src/Components/TitleBar.js
+++ src/Components/TitleBar.js
@@ -1,10 +1,12 @@
 import { Box, Heading } from "@chakra-ui/react";
 import React from "react";
+import InputForm from "./InputForm";
 
 export default function TitleBar() {
   return (
     <Box align="center">
       <Heading>Housing Pricing Predictor (California)</Heading>
+      <InputForm />
     </Box>
   );
 }
