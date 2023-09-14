2156f99094d51ed111aef0f0a907b70861a74d59
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-09-14 10:31:43 +0530

    Init and file setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import TitleBar from "./Components/TitleBar";
+
+export default function App() {
+  return (
+    <Box>
+      <TitleBar />
+    </Box>
+  );
+}
