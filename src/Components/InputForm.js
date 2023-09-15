38cd68d45863997cd329629d38f2bfc71eee88e6
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-09-15 11:29:43 +0530

    Update InputForm.js
    - Adding state management for input
    - updated default values
    - handled submit and change in input

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -7,19 +7,30 @@ import {
   Box,
   Select,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 
 export default function InputForm() {
   const initialHousingDetails = {
     latitude: "",
     longitude: "",
-    housing_median_age: 0,
-    total_rooms: 0,
-    total_bedrooms: 0,
-    population: 0,
-    households: 0,
-    median_income: 0,
-    ocean_proximity: "",
+    housing_median_age: "",
+    total_rooms: "",
+    total_bedrooms: "",
+    population: "",
+    households: "",
+    median_income: "",
+    ocean_proximity: "NEAR BAY",
+  };
+  const [housingDetails, sethousingDetails] = useState(initialHousingDetails);
+  const onDetailsChange = (event) => {
+    sethousingDetails({
+      ...housingDetails,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(housingDetails);
   };
   return (
     <Container>
@@ -28,39 +39,91 @@ export default function InputForm() {
           <FormLabel fontSize={"larger"} mx={1.5}>
             Latitude:
           </FormLabel>
-          <Input type="text" placeholder="Latitude" />
+          <Input
+            type="text"
+            placeholder="Latitude"
+            name="latitude"
+            value={housingDetails.latitude}
+            onChange={onDetailsChange}
+          />
           <FormLabel fontSize={"larger"} mx={1.5}>
             Longitude:
           </FormLabel>
-          <Input type="text" placeholder="Longitude" />
+          <Input
+            type="text"
+            placeholder="Longitude"
+            name="longitude"
+            value={housingDetails.longitude}
+            onChange={onDetailsChange}
+          />
         </InputGroup>
         <Box>
           <FormLabel>Housing Median Age :</FormLabel>
-          <Input type="number" placeholder="99" />
+          <Input
+            type="number"
+            placeholder="99"
+            name="housing_median_age"
+            value={housingDetails.housing_median_age}
+            onChange={onDetailsChange}
+          />
         </Box>
         <Box>
           <FormLabel>Total rooms :</FormLabel>
-          <Input type="number" placeholder="99" />
+          <Input
+            type="number"
+            placeholder="99"
+            name="total_rooms"
+            value={housingDetails.total_rooms}
+            onChange={onDetailsChange}
+          />
         </Box>
         <Box>
           <FormLabel>Total bedrooms :</FormLabel>
-          <Input type="number" placeholder="99" />
+          <Input
+            type="number"
+            placeholder="99"
+            name="total_bedrooms"
+            value={housingDetails.total_bedrooms}
+            onChange={onDetailsChange}
+          />
         </Box>
         <Box>
           <FormLabel>Population :</FormLabel>
-          <Input type="number" placeholder="99" />
+          <Input
+            type="number"
+            placeholder="99"
+            name="population"
+            value={housingDetails.population}
+            onChange={onDetailsChange}
+          />
         </Box>
         <Box>
           <FormLabel>Households :</FormLabel>
-          <Input type="number" placeholder="126" />
+          <Input
+            type="number"
+            placeholder="126"
+            name="households"
+            value={housingDetails.households}
+            onChange={onDetailsChange}
+          />
         </Box>
         <Box>
           <FormLabel>Median Income :</FormLabel>
-          <Input type="number" placeholder="8.3252" />
+          <Input
+            type="number"
+            placeholder="8.3252"
+            name="median_income"
+            value={housingDetails.median_income}
+            onChange={onDetailsChange}
+          />
         </Box>
         <Box>
           <FormLabel>Ocean Proximity:</FormLabel>
-          <Select>
+          <Select
+            name="ocean_proximity"
+            value={housingDetails.ocean_proximity}
+            onChange={onDetailsChange}
+          >
             <option>NEAR BAY</option>
             <option>NEAR OCEAN</option>
             <option>less than 1hr OCEAN</option>
@@ -68,6 +131,7 @@ export default function InputForm() {
             <option>ISLAND</option>
           </Select>
         </Box>
+        <Input type="submit" onClick={handleSubmit} />
       </Stack>
     </Container>
   );
