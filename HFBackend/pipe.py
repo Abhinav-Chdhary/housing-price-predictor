cb87628a2578c7db9eb8642fe88e72672501ffee
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-09-27 18:56:47 +0530

    Update pipe.py
    Just trying out stuff with hugging face

--- HFBackend/pipe.py
+++ HFBackend/pipe.py
@@ -3,13 +3,13 @@ from transformers import pipeline
 """ classifier = pipeline("sentiment-analysis")
 
 res = classifier("I do not think so")
-"""
+ """
 generator = pipeline("text-generation", model="distilgpt2")
 
 res = generator(
-    "In this course we will teach you how to ",
+    "Hi How are you?",
     max_length=30,
-    max_return_sequences=2,
+    num_return_sequences=2,
 )
 
 print(res)
