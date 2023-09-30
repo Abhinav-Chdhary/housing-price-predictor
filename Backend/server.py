86ecd9309cfdeba5e7427a730472ab6c3f256893
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-09-30 08:22:22 +0530

    Update server.py
    - User data preprocessing
    - Prediction based on user data

--- Backend/server.py
+++ Backend/server.py
@@ -7,6 +7,29 @@ app = Flask(__name__)
 CORS(app)
 
 
+def preprocessor(mytest, data):
+    mytest[0, 0] = int(data["latitude"])
+    mytest[0, 1] = int(data["longitude"])
+    mytest[0, 2] = int(data["housing_median_age"])
+    mytest[0, 3] = int(data["total_rooms"])
+    mytest[0, 4] = int(data["total_bedrooms"])
+    mytest[0, 5] = int(data["population"])
+    mytest[0, 6] = int(data["households"])
+    mytest[0, 7] = float(data["median_income"])
+    if data["ocean_proximity"] == "less than 1hr OCEAN":
+        mytest[0, 8] = 1
+    elif data["ocean_proximity"] == "INLAND":
+        mytest[0, 9] = 1
+    elif data["ocean_proximity"] == "ISLAND":
+        mytest[0, 10] = 1
+    elif data["ocean_proximity"] == "NEAR BAY":
+        mytest[0, 11] = 1
+    elif data["ocean_proximity"] == "NEAR OCEAN":
+        mytest[0, 12] = 1
+    mytest[0, 13] = int(data["total_bedrooms"]) / int(data["total_rooms"])
+    mytest[0, 14] = int(data["total_rooms"]) / int(data["households"])
+
+
 # sample route for testing
 @app.route("/")
 def helloWorld():
@@ -51,7 +74,7 @@ mytest = np.array(
 @app.route("/api/predict", methods=["POST"])
 def predictor():
     data = request.get_json()
-    print(data)
+    preprocessor(mytest, data)
     try:
         predictions = model.predict(mytest)
         return jsonify({"predictions": predictions.tolist()})
